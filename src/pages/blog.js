37d2984a2fbfc463382866f786ddf95b019fd885
Adrian Murage <adrianmurage21@gmail.com>
2022-01-11 21:49:37 +0300

    add the blog route
    add a sample mdx blog post

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const BlogPage = () =>{
+    return(
+        <Layout pageTitle="Blog">
+            <p>Posts go in here</p>
+        </Layout>
+    )
+}
+
+export default BlogPage
