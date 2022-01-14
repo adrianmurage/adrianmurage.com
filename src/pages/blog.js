0b0231d57ae22361552977923dd03c9d494af3dc
Adrian Murage <adrianmurage21@gmail.com>
2022-01-14 23:54:26 +0300

    Add support for mdx blog content

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const BlogPage = ({data}) =>{
     return(
         <Layout pageTitle="Blog">
             <ul>
             {
-            data.allFile.nodes.map(node => (
-                <li key={node.name}>
-                    {node.name}
-                </li>
+            data.allMdx.nodes.map((node) => (
+              <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2> 
+                <p> Podsted: {node.frontmatter.date}</p>
+                <MDXRenderer>
+                  {node.body}
+                </MDXRenderer>
+              </article>
             ))
             }
             </ul>
@@ -18,13 +23,18 @@ const BlogPage = ({data}) =>{
     )
 }
 
-export const allPostsQuery = graphql`
-query{
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+export const query = graphql`
+  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "D MMMM, YYYY")
+        }
+        id
+        body
       }
     }
   }
-  `
+`
 export default BlogPage
