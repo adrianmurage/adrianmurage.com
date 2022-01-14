6e3a49f56d3d0f5f386a2cfe8f8a191d644d5391
Adrian Murage <adrianmurage21@gmail.com>
2022-01-15 00:44:12 +0300

    changes to /blog/
    create a /blog dir that houses the blog page
    and dynamic blog post information

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+const BlogPost = () => {
+    return (
+        <Layout pageTitle="blog posts">
+            <p>My post content will go here</p>
+        </Layout>
+    )
+}
+
+export default BlogPost
