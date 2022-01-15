4db70556ff7b22eebb4aff41791567902a3e915a
Adrian Murage <adrianmurage21@gmail.com>
2022-01-15 21:09:48 +0300

    add support for hero images
    install gatsby-transformer-sharp

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,28 +2,49 @@ import React from "react"
 import Layout from "../../components/layout"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const BlogPost = ({data}) => {
+const BlogPost = (props) => {
+
+    const image = getImage(props.data.mdx.frontmatter.hero_image)
+
     return (
-        <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.date}</p>
+        <Layout pageTitle={props.data.mdx.frontmatter.title}>
+            <p>Posted: {props.data.mdx.frontmatter.date}</p>
+            <GatsbyImage image={image} alt={props.data.mdx.frontmatter.hero_image_alt}/>
+            <p>
+                Photo Credit:{" "}
+                <a href={props.data.mdx.frontmatter.hero_image_credit_link}>
+                    {props.data.mdx.frontmatter.hero_image_credit_text}
+                </a>
+            </p>
             <MDXRenderer>
-                {data.mdx.body}
+                {props.data.mdx.body}
             </MDXRenderer>
         </Layout>
     )
 }
 
 export const query = graphql`
-    query ($id: String) {
-        mdx(id: {eq: $id}){
-            frontmatter {
-                title
-                date(formatString: "D MMMM, YYYY")
-            }
-            body
+query ($id: String) {
+    mdx(id: {eq: $id}) {
+      id
+      frontmatter {
+        title
+        date(formatString: "D MMMM, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
         }
+      }
+      body
     }
+  }
+  
 `
 
 export default BlogPost
