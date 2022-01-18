37109f9e56b3c358a57482d7a69eb84a23c08d3f
Adrian Murage <adrianmurage21@gmail.com>
2022-01-18 23:51:29 +0300

    add syntax highlighting to code snippets

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwl"
 
 const components = {
-    pre: props => <div {...props} />,
-    code: props=> <pre style={{color: 'pink'}} {...props} />
-}
+  pre: (props) => {
+    console.log(props);
+    const className = props.children.props.className || "";
+    const code = props.children.props.children.trim();
+    const language = className.replace(/language-/, "");
 
-export const wrapRootElement = ({element}) => {
-    return <MDXProvider components={components}>{element}</MDXProvider>
-}
+    return (
+      <Highlight {...defaultProps} code={code} language={language} theme={theme}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    );
+  },
+  wrapper: ({ children }) => <>{children}</>,
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>;
+};
